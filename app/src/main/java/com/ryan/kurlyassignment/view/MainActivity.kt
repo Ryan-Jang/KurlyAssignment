f3ce7b3a31962c573144ef3b907f267d84ad648e
Ryan <Jangyh0904@naver.com>
2021-07-04 15:31:52 +0900

    #8 검색결과 리스트 관련 코드 구현 및 검색버튼 리스너 구현 issue close #8

--- app/src/main/java/com/ryan/kurlyassignment/view/MainActivity.kt
+++ app/src/main/java/com/ryan/kurlyassignment/view/MainActivity.kt
@@ -2,19 +2,23 @@ package com.ryan.kurlyassignment.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ryan.kurlyassignment.R
+import com.ryan.kurlyassignment.model.SearchModel
+import com.ryan.kurlyassignment.viewmodel.SearchViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var etSearch : EditText
     private lateinit var btnSearch : Button
     private lateinit var rvSearch : RecyclerView
     private lateinit var tvNoResult : TextView
     private lateinit var pbLoading : ProgressBar
+
+    private lateinit var searchAdapter : SearchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +29,42 @@ class MainActivity : AppCompatActivity() {
         rvSearch = findViewById(R.id.rvSearch)
         tvNoResult = findViewById(R.id.tvNoResult)
         pbLoading = findViewById(R.id.pbLoading)
+
+        btnSearch.setOnClickListener(this)
+
+        initList()
+    }
+
+    private fun initList() {
+        searchAdapter = SearchAdapter()
+        rvSearch.adapter = searchAdapter
+        rvSearch.addItemDecoration(
+            DividerItemDecoration(this, LinearLayout.VERTICAL).apply {
+                getDrawable(R.drawable.shape_divider)?.let {
+                    setDrawable(it)
+                }
+        })
+        rvSearch.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val lastVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition()
+                val itemTotalCount = recyclerView.adapter!!.itemCount -1
+
+                if (lastVisibleItemPosition == itemTotalCount) {
+
+                }
+            }
+        })
+    }
+
+    private fun requestRepoList() {
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btnSearch -> requestRepoList()
+        }
     }
 }
