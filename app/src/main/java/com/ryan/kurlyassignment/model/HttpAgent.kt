45d97d23a9d28d695d02d5c38b6c8141e6f461c3
Ryan <Jangyh0904@naver.com>
2021-07-04 18:44:35 +0900

    #22 HttpAgent 수정 issue close #22

--- app/src/main/java/com/ryan/kurlyassignment/model/HttpAgent.kt
+++ app/src/main/java/com/ryan/kurlyassignment/model/HttpAgent.kt
@@ -15,13 +15,15 @@ import java.io.IOException
 open class HttpAgent {
     companion object {
         var baseUrl : String = "https://api.github.com/search/"
-        private lateinit var httpClient : OkHttpClient.Builder
-        private lateinit var retrofitClient : Retrofit
-        private lateinit var requestService : SearchService
+        private var httpClient : OkHttpClient.Builder? = null
+        private var retrofitClient : Retrofit? = null
+        private var requestService : SearchService? = null
+    }
 
-        private fun getOkHttpClient(): OkHttpClient {
+    private fun getOkHttpClient(): OkHttpClient {
+        if (httpClient == null) {
             httpClient = OkHttpClient.Builder()
-            httpClient.addInterceptor(object : Interceptor {
+            httpClient?.addInterceptor(object : Interceptor {
                 @Throws(IOException::class)
                 override fun intercept(chain: Interceptor.Chain): Response {
                     val requestBuilder : Request.Builder = chain.request().newBuilder()
@@ -31,24 +33,26 @@ open class HttpAgent {
                 }
             })
 
-            httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-
-            return httpClient.build()
+            httpClient?.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         }
 
-        private fun getRetrofitInstance() : Retrofit {
+        return httpClient!!.build()
+    }
+
+    private fun getRetrofitInstance() : Retrofit {
+        if (retrofitClient == null)
             retrofitClient = Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(getOkHttpClient())
                 .build()
-            return retrofitClient
-        }
+        return retrofitClient!!
+    }
 
-        fun getRequestServiceInstance() : SearchService {
+    fun getRequestServiceInstance() : SearchService {
+        if (requestService == null)
             requestService = getRetrofitInstance().create(SearchService::class.java)
-            return requestService
-        }
+        return requestService!!
     }
 
     interface SearchService {
